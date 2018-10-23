0873276c2d41666ab2162ba5837aa2987a3f82f7
Eric Zhou <42453465+TheZooo@users.noreply.github.com>
2018-10-23 12:29:03 -1000

    One Room Working

--- hotelScript.js
+++ hotelScript.js
@@ -1,5 +1,6 @@
 var selector = 0;
 var count = [0, 0, 0, 0, 0, 0];
+var max = 4;
 
 function selOne() {
   selector = 0;
@@ -10,80 +11,115 @@ function lock() {
   var div = document.getElementsByClassName('display')[selector];
   if (count[selector] === 0) {
     count[selector] = count[selector] - 1;
-    var elem = document.createElement('h1');
-    var txt = document.createTextNode("Closed For Cleaning");
-    elem.appendChild(txt);
-    div.appendChild(elem);
+    document.getElementsByTagName('h1')[selector].style.display = "block";
     document.getElementsByClassName('clean')[selector].innerHTML = "Open from Cleaning";
     document.getElementsByClassName('display')[selector].style.backgroundColor = "#B48B7D";
   }
   else if (count[selector] === -1) {
     count[selector] = count[selector] + 1;
-    var h = document.getElementsByTagName('h1')[selector];
-    div.removeChild(h);
+    document.getElementsByTagName('h1')[selector].style.display = "none";
     document.getElementsByClassName('clean')[selector].innerHTML = "Close for Cleaning";
     document.getElementsByClassName('display')[selector].style.backgroundColor = "#FFF9A5";
   }
 }
 
+
+
 function add() {
   var t = document.getElementsByClassName('inIn')[selector].value;
+  var i = document.getElementsByTagName('li').length;
+  var ii = 0;
+  var x = document.getElementsByTagName('li')[0 + (max * selector)].innerHTML;
+  var y = document.getElementsByTagName('li')[1 + (max * selector)].innerHTML;
+  var z = document.getElementsByTagName('li')[2 + (max * selector)].innerHTML;
+  var w = document.getElementsByTagName('li')[3 + (max * selector)].innerHTML;
+  var ul = document.getElementsByClassName('chek')[selector];
+  var li;
   
+  for (x,i; x === "" && i > 0; i--) {
+    if (x === "") {
+      li = document.getElementsByTagName('li')[0 + (max * selector)];
+      ul.removeChild(li);
+    }
+  }
+  for (y,i; y === "" && i > 1; i--) {
+    if (y === "") {
+      li = document.getElementsByTagName('li')[1 + (max * selector)];
+      ul.removeChild(li);
+    }
+  }
+  for (z,i; z === "" && i > 2; i--) {
+    if (z === "") {
+      li = document.getElementsByTagName('li')[2 + (max * selector)];
+      ul.removeChild(li);
+    }
+  }
+  if (w === "" && i > 3) {
+    li = document.getElementsByTagName('li')[3 + (max * selector)];
+    ul.removeChild(li);
+  }
+
   if (t === "") {}
-  
-  else if (count[selector] < 4 && count[selector] > 0) {
+  else if (count[selector] < max && count[selector] >= 0) {
     var elem = document.createElement('li');
     var nTxt = document.createTextNode(t + " checked in");
-    var ul = document.getElementsByClassName('chek')[selector];
     elem.appendChild(nTxt);
     ul.appendChild(elem);
     count[selector] = count[selector] + 1;
     document.getElementsByClassName('display')[selector].style.backgroundColor = "#FFF9A5";
   }
   
-  if (count[selector] >= 0) {
-    document.getElementsByClassName('remove')[selector].style.visibility = "visible";
+  for (ii; ii < (max - count[selector]);ii++) {
+    var mlem = document.createElement('li');
+    ul.appendChild(mlem);
   }
   
+  if (count[selector] > 0) {
+    document.getElementsByClassName('remove')[selector].style.visibility = "visible";
+  }
   if (count[selector] === 0) {
     document.getElementsByClassName('remove')[selector].style.visibility = "hidden";
   }
-  
-  if (count[selector] === 4) {
-    document.getElementsByClassName('inIn')[selector].value = "";
+  if (count[selector] === max) {
     document.getElementsByClassName('display')[selector].style.backgroundColor = "#B48B7D";
   }
   document.getElementsByClassName('inIn')[selector].value = "";
 }
 
+
+
 function rmv() {
   var i = document.getElementsByClassName('inOut')[selector].value - 1;
   var ul = document.getElementsByClassName('chek')[selector];
-  window.alert(i);
   
-  if (i < 0 || i > 4) {}
+  if (i < 0 || i > max) {}
   else if (i >= 0) {
-    var il = document.getElementsByTagName('li')[0 + (4 * selector) + i];
+    var elem = document.createElement('li');
+    var il = document.getElementsByTagName('li')[0 + (max * selector) + i];
     ul.removeChild(il);
-    
     count[selector] = count[selector] - 1;
+    ul.appendChild(elem);
+    document.getElementsByClassName('display')[selector].style.backgroundColor = "#FFF9A5";
   }
   if (count[selector] === 0) {
     document.getElementsByClassName('remove')[selector].style.visibility = "hidden";
+    document.getElementsByClassName('display')[selector].style.backgroundColor = "#FFF9A5";
   }
   document.getElementsByClassName('inOut')[selector].value = "";
-  document.getElementsByClassName('display')[selector].style.backgroundColor = "#FFF9A5";
+  
 }
+
+
 
 function rmvAll() {
   var i = 0;
-  var amt = count[selector];
   var ul = document.getElementsByClassName('chek')[selector];
   
-  for (i; i <= amt; i++) {
-    var il = document.getElementsByTagName('li')[0 + (4 * selector)];
-    window.alert(4 * selector);
-    ul.removeChild(il);
+  for (i; i <= max; i++) {
+    var li = document.getElementsByTagName('li')[0 + (max * selector)];
+    var elem = document.createElement('li');
+    ul.removeChild(li);
+    ul.appendChild(elem);
     count[selector] = 0;
     document.getElementsByClassName('remove')[selector].style.visibility = "hidden";
     document.getElementsByClassName('display')[selector].style.backgroundColor = "#FFF9A5";
